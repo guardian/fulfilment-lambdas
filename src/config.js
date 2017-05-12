a68531341e9af6e1687ed52193426d52470650d3
Alex Ware <alex.ware@guardian.co.uk>
2017-05-12 15:50:24 +0100

    Lint all the files with standardjs (standardjs.com)

--- src/config.js
+++ src/config.js
@@ -1,29 +1,27 @@
 import AWS from 'aws-sdk'
-
+import NamedError from './NamedError'
 let s3 = new AWS.S3()
 
-export function fetchConfig() {
-    return new Promise((resolve, reject) => {
-        let stage = process.env.Stage;
-        if (stage != 'CODE' && stage != 'PROD') {
-            reject(`invalid stage: ${stage}, please fix Stage env variable`);
-            return;
-        }
-        const key = 'fulfilment.private.json';
-        const bucket = `fulfilment-private/${stage}`;
-        console.log(`loading ${stage} configuration from ${bucket}/${key}`);
+export function fetchConfig () {
+  return new Promise((resolve, reject) => {
+    let stage = process.env.Stage
+    if (stage !== 'CODE' && stage !== 'PROD') {
+      reject(new Error(`invalid stage: ${stage}, please fix Stage env variable`))
+      return
+    }
+    const key = 'fulfilment.private.json'
+    const bucket = `fulfilment-private/${stage}`
+    console.log(`loading ${stage} configuration from ${bucket}/${key}`)
 
-        s3.getObject(
+    s3.getObject(
             { Bucket: bucket, Key: key },
             function (err, data) {
-                if (err)
-                    reject(getError('s3_download_error', `Error fetching config for S3 : ${err}`));
-                else {
-                    const json = JSON.parse(new Buffer(data.Body));
-                    var config = json.zuora.api;
-                    config.stage = stage;
-                    resolve(config);
-                }
-            });
-    })
+              if (err) { reject(new NamedError('s3_download_error', `Error fetching config for S3 : ${err}`)) } else {
+                const json = JSON.parse(Buffer.from(data.Body))
+                var config = json.zuora.api
+                config.stage = stage
+                resolve(config)
+              }
+            })
+  })
 }
--- src/querier.js
+++ src/querier.js
@@ -1,101 +1,98 @@
 import {fetchConfig} from './config'
-import request  from 'request'
+import request from 'request'
 import moment from 'moment'
 
-function queryZuora(deliveryDate, config){
-	let promise = new Promise((resolve, reject) => {
-		const deliveryDay = moment(deliveryDate,'yyyy-mm-dd').format('dddd');
-		const subsQuery = `
-				SELECT 
-			    	RateplanCharge.quantity,
-			    	Subscription.Name,
-			      	SoldToContact.Address1,
-			      	SoldToContact.Address2,
-			      	SoldToContact.City, 
-			      	SoldToContact.Country, 
-			      	SoldToContact.FirstName, 
-			      	SoldToContact.LastName, 
-			      	SoldToContact.PostalCode, 
-			      	SoldToContact.State,
-			      	SoldToContact.workPhone 
-			    FROM 
-			    	rateplancharge 
-			    WHERE
-			     (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
-			     ProductRatePlanCharge.ProductType__c = 'Print ${deliveryDay}' AND 
-			     Product.Name = 'Newspaper Delivery' AND 
-			     RatePlanCharge.EffectiveStartDate <= '${deliveryDate}' AND
-			     RatePlanCharge.EffectiveEndDate >= '${deliveryDate}' AND
-			     RatePlanCharge.MRR != 0`;
+function queryZuora (deliveryDate, config) {
+  let promise = new Promise((resolve, reject) => {
+    const deliveryDay = moment(deliveryDate, 'yyyy-mm-dd').format('dddd')
+    const subsQuery = `
+        SELECT 
+            RateplanCharge.quantity,
+            Subscription.Name,
+              SoldToContact.Address1,
+              SoldToContact.Address2,
+              SoldToContact.City, 
+              SoldToContact.Country, 
+              SoldToContact.FirstName, 
+              SoldToContact.LastName, 
+              SoldToContact.PostalCode, 
+              SoldToContact.State,
+              SoldToContact.workPhone 
+          FROM 
+            rateplancharge 
+          WHERE
+           (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
+           ProductRatePlanCharge.ProductType__c = 'Print ${deliveryDay}' AND 
+           Product.Name = 'Newspaper Delivery' AND 
+           RatePlanCharge.EffectiveStartDate <= '${deliveryDate}' AND
+           RatePlanCharge.EffectiveEndDate >= '${deliveryDate}' AND
+           RatePlanCharge.MRR != 0`
 
-	 	const holidaySuspensionQuery = `
-			SELECT 
-				Subscription.Name
-			FROM 
-				rateplancharge 
-			WHERE
-			 (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
-			 ProductRatePlanCharge.ProductType__c = 'Adjustment' AND 
-			 RateplanCharge.Name = 'Holiday Credit' AND 
-			 RatePlanCharge.EffectiveStartDate <= '${deliveryDate}' AND
-			 RatePlanCharge.HolidayEnd__c >= '${deliveryDate}'`;
+    const holidaySuspensionQuery = `
+      SELECT 
+        Subscription.Name
+      FROM 
+        rateplancharge 
+      WHERE
+       (Subscription.Status = 'Active' OR Subscription.Status = 'Cancelled') AND
+       ProductRatePlanCharge.ProductType__c = 'Adjustment' AND 
+       RateplanCharge.Name = 'Holiday Credit' AND 
+       RatePlanCharge.EffectiveStartDate <= '${deliveryDate}' AND
+       RatePlanCharge.HolidayEnd__c >= '${deliveryDate}'`
 
-		const options = {                 
-		  method: 'POST',             
-		  uri: `${config.url}/apps/api/batch-query/`,   
-		  json: true,   
-		  body: {
-		  "format": "csv",
-		  "version": "1.0",
-		  "name": "Fulfilment-Queries",
-		  "encrypted": "none",
-		  "useQueryLabels": "true",
-		  "dateTimeUtc": "true",
-		  "queries": [
-		    {
-		      "name": "Subscriptions",
-		      "query": subsQuery ,
-		      "type": "zoqlexport"
-		    }
-		    ,
-		    {
-		      "name": "HolidaySuspensions",
-		      "query": holidaySuspensionQuery,
-		      "type": "zoqlexport"
-		    }
-		  ]
-		  },                
-		  headers: {               
-		    'Authorization': 'Basic ' + new Buffer(`${config.username}:${config.password}`).toString('base64'),
-		    'Content-Type': 'application/json'                  
-		  }
-		};  
+    const options = {
+      method: 'POST',
+      uri: `${config.url}/apps/api/batch-query/`,
+      json: true,
+      body: {
+        'format': 'csv',
+        'version': '1.0',
+        'name': 'Fulfilment-Queries',
+        'encrypted': 'none',
+        'useQueryLabels': 'true',
+        'dateTimeUtc': 'true',
+        'queries': [
+          {
+            'name': 'Subscriptions',
+            'query': subsQuery,
+            'type': 'zoqlexport'
+          },
+          {
+            'name': 'HolidaySuspensions',
+            'query': holidaySuspensionQuery,
+            'type': 'zoqlexport'
+          }
+        ]
+      },
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(`${config.username}:${config.password}`).toString('base64'),
+        'Content-Type': 'application/json'
+      }
+    }
 
-		request(options, function(error, response, body) {  
-		if (error) {
-			reject(error);
-			return;
-			}	
+    request(options, function (error, response, body) {
+      if (error) {
+        reject(error)
+        return
+      }
 
-		  console.log('statusCode:', response && response.statusCode); 
-		  console.log('body:', body); 
-		  if(response.statusCode != '200') {
-		  		reject(`error response status ${response.statusCode}`);
-			} else if(body.errorCode) {
-				reject(`zuora error! code: ${body.errorCode} : ${body.message}`)
-			} else {
-				resolve(body.id);
-			}
-		});
-	});
-	return promise;
+      console.log('statusCode:', response && response.statusCode)
+      console.log('body:', body)
+      if (response.statusCode !== '200') {
+        reject(new Error(`error response status ${response.statusCode}`))
+      } else if (body.errorCode) {
+        reject(new Error(`zuora error! code: ${body.errorCode} : ${body.message}`))
+      } else {
+        resolve(body.id)
+      }
+    })
+  })
+  return promise
 }
 
-
-
-export function handler(input, context, callback){
-	fetchConfig()
-	.then(config=> queryZuora(input.deliveryDate, config))
-	.then(jobId => callback(null, {"jobId":jobId, "deliveryDate":input.deliveryDate}))
-	.catch(error => callback(error));
+export function handler (input, context, callback) {
+  fetchConfig()
+  .then(config => queryZuora(input.deliveryDate, config))
+  .then(jobId => callback(null, {'jobId': jobId, 'deliveryDate': input.deliveryDate}))
+  .catch(error => callback(error))
 }
