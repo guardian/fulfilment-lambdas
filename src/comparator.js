7555c355a861cdfa489f77cc6640e1f187326806
Alex Ware <alex.ware@guardian.co.uk>
2017-05-19 11:56:33 +0100

    Compare salesforce files to our files.

--- src/comparator.js
+++ src/comparator.js
@@ -1,10 +1,8 @@
 import AWS from 'aws-sdk'
 import csv from 'fast-csv'
 import {fetchConfig} from './config'
-import union from 'lodash/union'
-import difference from 'lodash/difference'
-import type ReadableStream from 'lib/streams'
-import stream from 'stream'
+import intersectionWith from 'lodash/intersectionWith'
+import differenceWith from 'lodash/differenceWith'
 
 type S3Path = {
   Bucket: string,
@@ -52,36 +50,56 @@ async function compare () {
   const logkeys = logresp.Contents.map(r => { return r.Key.slice(guprefix.length) })
   console.log('Found the following log files', logkeys)
 
-  const joint = union(sfkeys, gukeys)
+  const joint = intersectionWith(gukeys, sfkeys, (a:string, b:string) => {
+    return a.split('_').join('') === b.split('_').join('')
+  })
   console.log('In both systems', joint)
 
-  const unchecked = difference(joint, logkeys)
+  const unchecked = differenceWith(joint, logkeys, (a, b) => {
+    return a === logFileNameFor(b)
+  })
   console.log('remaining to check', unchecked)
 
-  let sfPath = {Bucket: bucket, Key: `${sfprefix}${unchecked[0]}`}
+  if (unchecked.length === 0) {
+    return 'No files found to check.'
+  }
+
+  let sfPath = {Bucket: bucket, Key: `${sfprefix}${sfFilenameFor(unchecked[0])}`}
   let guPath = {Bucket: bucket, Key: `${guprefix}${unchecked[0]}`}
+  let logPath = {Bucket: bucket, Key: `${logprefix}${logFileNameFor(unchecked[0])}`}
+
+  let logCache = []
+
+  let log = (entry) => {
+    console.log(entry)
+    logCache.push(entry)
+  }
 
   let sfOutput = await fetchCSV(sfPath)
   let guOutput = await fetchCSV(guPath)
 
   Object.keys(sfOutput).forEach((id) => {
     if (guOutput[id] === undefined || guOutput[id] == null) {
-      console.log(`Subscription ${id} found in Salesforce Output, but not fulfilment file.`)
+      log(`Subscription ${id} found in Salesforce Output, but not fulfilment file.`)
       return
     }
     let s = sfOutput[id]
     let g = guOutput[id]
     if (s.length !== g.length) {
-      console.log(`Differing numbers of fulfilments generated for ${id}. Salesforce: ${s.length} File: ${g.length}`)
+      log(`Differing numbers of fulfilments generated for ${id}. Salesforce: ${s.length} File: ${g.length}`)
     }
     delete guOutput[id]
   })
   Object.keys(guOutput).forEach((id) => {
-    console.log(`${id} found in fulfilment file but not in Salesforce output.`)
+    log(`${id} found in fulfilment file but not in Salesforce output.`)
   })
-
-  return null
-//  console.log(stream)
+  log(null)
+  return s3.upload({
+    ACL: 'private',
+    ServerSideEncryption: 'aws:kms',
+    Body: logCache.join('\n'),
+    ...logPath
+  }).promise()
 }
 
 function notEmpty (str) {
@@ -90,6 +108,7 @@ function notEmpty (str) {
 
 function fetchCSV (path: S3Path) {
   let customers = {}
+  console.log(`Fetching ${path.Key} from ${path.Bucket}`)
   let csvStream = s3.getObject(path).createReadStream()
   console.log('hey :)')
   return new Promise((resolve, reject) => {
@@ -108,4 +127,19 @@ function fetchCSV (path: S3Path) {
     csvStream.pipe(reader)
     console.log('piped')
   })
+}
+
+function sfFilenameFor (filename:string) {
+  let arr = filename.split('_')
+  /* From
+   * 'HOME_DELIVERY_Wednesday_25_01_2017.csv'
+   *   0      1       2        3  4   5
+   * To
+   * 'HOME_DELIVERY_Wednesday25_01_2017.csv'
+   */
+  return `${arr[0]}_${arr[1]}_${arr[2]}${arr[3]}_${arr[4]}_${arr[5]}`
+}
+
+function logFileNameFor (filename: string) {
+  return filename.replace('csv', 'log')
 }
