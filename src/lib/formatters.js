54013832a980213f2d7d73f9ef22f46962701512
Alex Ware <alex.ware@guardian.co.uk>
2017-06-20 17:15:06 +0100

    use rn naming convention for the postcode constituents

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -6,7 +6,7 @@ export function formatPostCode (postCode: string) {
    */
   let normalised = postCode.replace(/ /g, '').toUpperCase()
   let length = normalised.length
-  let head = normalised.substring(0, length - 3)
-  let tail = normalised.substring(length - 3)
-  return `${head} ${tail}`
+  let outward = normalised.substring(0, length - 3)
+  let inward = normalised.substring(length - 3)
+  return `${outward} ${inward}`
 }
