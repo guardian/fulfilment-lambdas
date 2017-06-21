7d7840907515123875dd2243e98a87068d1e0fde
pvighi <patricio.vighi@guardian.co.uk>
2017-06-21 17:57:12 +0100

    added basic token authentication
    this should return status 401 if auth fails but for now it returns 500

--- src/fulfilmentTrigger.js
+++ src/fulfilmentTrigger.js
@@ -7,6 +7,7 @@ import AWS from 'aws-sdk'
 const stepfunctions = new AWS.StepFunctions();
 const DATE_FORMAT = "YYYY-MM-DD"
 const BAD_REQUEST = 400
+const UNAUTHORIZED = 401
 function getParams (date) {
   let params = {}
   params.stateMachineArn = process.env.StateMachine
@@ -48,32 +49,64 @@ function triggerFulfilmentFor(date: String) {
   })
 }
 
-function range(amount) {
+function triggerLambdas (startDate, amount) {
+  let results = range(amount).map(offset => {
+      let date = moment(startDate, DATE_FORMAT).add(offset, 'days').format(DATE_FORMAT)
+      return triggerFulfilmentFor(date)
+    }
+  )
+  return Promise.all(results)
+}
+
+function range (amount) {
   let resArray = []
   for (var i = 0; i < amount; i++) {
     resArray.push(i)
   }
   return resArray
 }
+
+
+
 export function handler (input, context, callback) {
   console.log(input)
   let body = JSON.parse(input.body)
   if (!body.amount || !body.date) {
-    callback(null, getErrorResponse(BAD_REQUEST, "missing amount or date"))
+    callback(null, getErrorResponse(BAD_REQUEST, 'missing amount or date'))
   }
-  if (body.amount < 1 || body.amount >10) {
-    callback(null, getErrorResponse(BAD_REQUEST, "amount should be a number between 1 and 10"))
+  if (body.amount < 1 || body.amount > 10) {
+    callback(null, getErrorResponse(BAD_REQUEST, 'amount should be a number between 1 and 10'))
   }
+  let providedToken = input.headers.apiToken
+  if (!providedToken){
+    callback(null, getErrorResponse(BAD_REQUEST, 'ApiToken missing from request'))
 
-   let results = range(body.amount).map(offset => {
-       let date = moment(body.date, DATE_FORMAT).add(offset, "days").format(DATE_FORMAT)
-       return triggerFulfilmentFor(date)
-     }
-   )
-  Promise.all(results)
+  }
+  asyncHandler(body.date, body.amount, providedToken)
     .then(res => {
       console.log(res)
       callback(null, okRes)
     })
     .catch(error => callback(serverError))
+}
+function validateToken (expectedToken, providedToken) {
+  return new Promise((resolve, reject) => {
+    if (expectedToken == providedToken) {
+      resolve()
+    }
+    else {
+      console.log("failed token authentication")
+      //TODO RETURN STATUS 401 IF THIS HAPPENS
+      reject('invalid token')
+    }
+  })
+}
+
+async function asyncHandler (startDate, amount, providedToken) {
+  let config = await fetchConfig()
+  console.log('Config fetched succesfully.')
+  await validateToken(config.triggerLambda.expectedToken, providedToken)
+  console.log("token validated successfully")
+  return triggerLambdas(startDate, amount)
+
 }
