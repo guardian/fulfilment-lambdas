b30492c488cf0c2b3f60a868f9dc43d98de65eb3
pvighi <patricio.vighi@guardian.co.uk>
2017-09-27 11:57:04 +0100

    guardian weekly test fixes

--- __tests__/weekly/exporter.js
+++ __tests__/weekly/exporter.js
@@ -3,7 +3,7 @@ import { handler } from '../../src/exporter'
 import { readFile } from 'fs'
 var MockDate = require('mockdate')
 
-let mockOutput = null
+let mockOutput = {}
 // mock current date
 MockDate.set('7/5/2017')
 
@@ -23,8 +23,9 @@ jest.mock('../../src/lib/storage', () => {
   const streamToString = require('stream-to-string')
 
   return {
-    upload: async (stream, outputLocation) => {
-      mockOutput[outputLocation] = await streamToString(stream)
+    upload: async (stream, outputLocation, folder) => {
+      let outputPath = folder.prefix + outputLocation.filename
+      mockOutput[outputPath] = await streamToString(stream)
       return outputLocation
     },
     createReadStream: (filePath) => {
@@ -37,7 +38,153 @@ jest.mock('../../src/lib/storage', () => {
 
 jest.mock('../../src/lib/config', () => ({
   getStage: () => 'CODE',
-  fetchConfig: async () => ({fulfilments: {homedelivery: {uploadFolder: ''}}})
+  fetchConfig: async () => (
+    {
+      fulfilments: {
+        weekly: {
+          VU: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_VU',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_VU/'
+            },
+            downloadFolder: {
+              folderId: 'folderId1',
+              name: 'Guardian Weekly (Vanatu)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/vu/'
+            }
+          },
+          HK: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_HK',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_HK/'
+            },
+            downloadFolder: {
+              folderId: 'folderId2',
+              name: 'Guardian Weekly (Hong Kong)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/hk/'
+            }
+          },
+          ROW: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_ROW',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_ROW/'
+            },
+            downloadFolder: {
+              folderId: 'folderId3',
+              name: 'Guardian Weekly (Rest of tge World)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/row/'
+            }
+          },
+          AU: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_AU',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_AU/'
+            },
+            downloadFolder: {
+              folderId: 'folderId4',
+              name: 'Guardian Weekly (Australia)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/au/'
+            }
+          },
+          US: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_US',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_US/'
+            },
+            downloadFolder: {
+              folderId: '00l0J000002OrHhQAK',
+              name: 'Guardian Weekly (USA)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/usa/'
+            }
+          },
+          FR: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_FR',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_FR/'
+            },
+            downloadFolder: {
+              folderId: 'folderId5',
+              name: 'Guardian Weekly (France)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/fr/'
+            }
+          },
+          NZ: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_NZ',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_NZ/'
+            },
+            downloadFolder: {
+              folderId: 'folderId6',
+              name: 'Guardian Weekly (New Zealand)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/nz/'
+            }
+          },
+          UK: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_UK',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_UK/'
+            },
+            downloadFolder: {
+              folderId: 'folderId7',
+              name: 'Guardian Weekly (UK)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/uk/'
+            }
+          },
+          CAHAND: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_CA_HAND',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_CA_HAND/'
+            },
+            downloadFolder: {
+              folderId: 'folderId8',
+              name: 'Guardian Weekly (Vanatu)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/ca_hand/'
+            }
+          },
+          CA: {
+            uploadFolder: {
+              folderId: null,
+              name: 'Weekly_Pipeline_CA',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/fulfilments/Weekly_CA/'
+            },
+            downloadFolder: {
+              folderId: 'folderId19',
+              name: 'Guardian Weekly (Canada)',
+              bucket: 'fulfilment-bucket-name',
+              prefix: 'TEST/salesforce_output/weekly/ca/'
+            }
+          }
+        }
+      }
+    })
 }))
 
 function verify (done, expectedError, expectedResponse, expectedFileNames) {
@@ -60,11 +207,12 @@ function verify (done, expectedError, expectedResponse, expectedFileNames) {
               done.fail(err)
               return
             }
-            expect(expectedContents).toEqual(mockOutput[expectedFileName])
+            expect(mockOutput[expectedFileName]).toEqual(expectedContents)
           })
           done()
         })
       }
+      done()
     } catch (error) {
       done.fail(error)
     }
@@ -73,7 +221,8 @@ function verify (done, expectedError, expectedResponse, expectedFileNames) {
 
 beforeEach(() => {
   process.env.Stage = 'CODE'
-  mockOutput = null
+  let mockOutput = {}
+
 })
 
 test('should return error on missing query subscriptions query result for weekly', done => {
@@ -124,9 +273,14 @@ test('should generate correct fulfilment file for weekly', done => {
       }
     ]
   }
-  let expectedFileName = '2017-07-06_WEEKLY_United_Kingdom.csv'
-  let expectedResponse = {...input, 'fulfilmentFile': expectedFileName}
 
-  handler(input, {}, verify(done, null, expectedResponse, [expectedFileName]))
-  done()
+  //todo this response maybe should include the full path
+  let expectedResponse = {...input, 'fulfilmentFile': '2017-07-06_WEEKLY.csv,2017-07-06_WEEKLY.csv,2017-07-06_WEEKLY.csv,2017-07-06_WEEKLY.csv,2017-07-06_WEEKLY.csv'}
+  let expectedFileNames = [
+    "TEST/fulfilments/Weekly_UK/2017-07-06_WEEKLY.csv",
+   "TEST/fulfilments/Weekly_CA/2017-07-06_WEEKLY.csv" ,
+   "TEST/fulfilments/Weekly_CA_HAND/2017-07-06_WEEKLY.csv",
+   "TEST/fulfilments/Weekly_US/2017-07-06_WEEKLY.csv",
+   "TEST/fulfilments/Weekly_AU/2017-07-06_WEEKLY.csv"]
+  handler(input, {}, verify(done, null, expectedResponse, expectedFileNames))
 })
