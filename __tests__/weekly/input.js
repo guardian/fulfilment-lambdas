f7715ef5ae829cada319b1096d8d001c39a4d58a
pvighi <patricio.vighi@guardian.co.uk>
2017-12-07 16:15:29 +0000

    added input params to weekly uploader lambda to be used in weekly scheduled execution

--- __tests__/weekly/input.js
+++ __tests__/weekly/input.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { getDeliveryDate } from '../../src/weekly/query'
+import { getDeliveryDate } from '../../src/weekly/input'
 import moment from 'moment'
 
 var MockDate = require('mockdate')
