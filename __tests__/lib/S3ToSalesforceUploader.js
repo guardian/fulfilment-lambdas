62eac354e2a095613e1dee12f8d7cf66081ed105
Mario Galic <mario.galic@guardian.co.uk>
2020-01-13 12:33:07 +0000

    Fix weekly upload tests

--- __tests__/lib/S3ToSalesforceUploader.js
+++ __tests__/lib/S3ToSalesforceUploader.js
@@ -70,8 +70,9 @@ test('should upload files', done => {
   })
 })
 
-test('should ignore missing files', done => {
+test('should NOT ignore missing files', async () => {
   const mockedStorage = require('../../src/lib/storage')
+  const expectedError = new Error('file not found')
   mockedStorage.getObject = jest.fn().mockImplementation(path => {
     if (path !== 'source_path/file_2') {
       return Promise.resolve({
@@ -80,32 +81,8 @@ test('should ignore missing files', done => {
         }
       })
     }
-    return Promise.reject(new Error('file not found'))
+    return Promise.reject(expectedError)
   })
   const testInput = [getTestInput(1), getTestInput(2), getTestInput(3)]
-  sfUpload.uploadFiles(testInput, mockSalesForce).then(res => {
-    try {
-      // downloads from s3
-      expect(mockedStorage.getObject.mock.calls.length).toBe(3)
-      expect(mockedStorage.getObject).toHaveBeenCalledWith('source_path/file_1')
-      expect(mockedStorage.getObject).toHaveBeenCalledWith('source_path/file_2')
-      expect(mockedStorage.getObject).toHaveBeenCalledWith('source_path/file_3')
-
-      // uploads to sf
-      expect(mockSalesForce.uploadDocument.mock.calls.length).toBe(2)
-
-      expect(mockSalesForce.uploadDocument).toHaveBeenCalledWith('sfFilename_1', { folderId: 'sfFolderId_1', name: 'sfFolderName1' }, 'sfDescription_1', undefined)
-      expect(mockSalesForce.uploadDocument).toHaveBeenCalledWith('sfFilename_3', { folderId: 'sfFolderId_3', name: 'sfFolderName3' }, 'sfDescription_3', undefined)
-
-      const expectedResponse = [
-        { id: 'documentId', name: 'sfFilename_1' },
-        { id: 'documentId', name: 'sfFilename_3' }
-      ]
-
-      expect(res).toEqual(expectedResponse)
-      done()
-    } catch (e) {
-      done.fail(e)
-    }
-  })
+  await expect(sfUpload.uploadFiles(testInput, mockSalesForce)).rejects.toThrow()
 })
