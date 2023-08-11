d42cf78270816b8d58804b1e57028b6257bacf26
Emily Bourke <emily.bourke@guardian.co.uk>
2023-08-11 13:47:56 +0100

    national delivery: Use both stages in cdk snapshot

--- national-delivery-fulfilment/cdk/lib/national-delivery-fulfilment.test.ts
+++ national-delivery-fulfilment/cdk/lib/national-delivery-fulfilment.test.ts
@@ -1,12 +1,21 @@
-import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { NationalDeliveryFulfilment } from "./national-delivery-fulfilment";
+import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { NationalDeliveryFulfilment } from './national-delivery-fulfilment';
 
-describe("The NationalDeliveryFulfilment stack", () => {
-  it("matches the snapshot", () => {
-    const app = new App();
-    const stack = new NationalDeliveryFulfilment(app, "NationalDeliveryFulfilment", { stack: "membership", stage: "TEST" });
-    const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
-  });
+describe('The NationalDeliveryFulfilment stack', () => {
+	it('matches the snapshot', () => {
+		const app = new App();
+		const codeStack = new NationalDeliveryFulfilment(
+			app,
+			'NationalDeliveryFulfilment-CODE',
+			{ stack: 'membership', stage: 'CODE' },
+		);
+		const prodStack = new NationalDeliveryFulfilment(
+			app,
+			'NationalDeliveryFulfilment-PROD',
+			{ stack: 'membership', stage: 'PROD' },
+		);
+		expect(Template.fromStack(codeStack).toJSON()).toMatchSnapshot();
+		expect(Template.fromStack(prodStack).toJSON()).toMatchSnapshot();
+	});
 });
